01e25b8d33c4e4e25f8108c08a07869c6f5e27ff
NoctuaCode <noctuaapps@gmail.com>
2024-10-16 11:25:25 +1100

    feat(QMK): Set keymap to real prog dvorak

--- keyboards/voyager/keymaps/default/keymap.c
+++ keyboards/voyager/keymaps/default/keymap.c
@@ -1,75 +1,104 @@
-// Copyright 2023 ZSA Technology Labs, Inc <@zsa>
-// Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 #include "version.h"
-
-
-#define KC_MAC_UNDO LGUI(KC_Z)
-#define KC_MAC_CUT LGUI(KC_X)
-#define KC_MAC_COPY LGUI(KC_C)
-#define KC_MAC_PASTE LGUI(KC_V)
-#define KC_PC_UNDO LCTL(KC_Z)
-#define KC_PC_CUT LCTL(KC_X)
-#define KC_PC_COPY LCTL(KC_C)
-#define KC_PC_PASTE LCTL(KC_V)
-#define ES_LESS_MAC KC_GRAVE
-#define ES_GRTR_MAC LSFT(KC_GRAVE)
-#define ES_BSLS_MAC ALGR(KC_6)
-#define NO_PIPE_ALT KC_GRAVE
-#define NO_BSLS_ALT KC_EQUAL
-#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
-#define BP_NDSH_MAC ALGR(KC_8)
-#define SE_SECT_MAC ALGR(KC_6)
+#include "features/custom_shift_keys.h"
 #define MOON_LED_LEVEL LED_LEVEL
+#define ML_SAFE_RANGE SAFE_RANGE
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
-  HSV_0_255_255,
-  HSV_74_255_255,
-  HSV_169_255_255,
-};
-
-
-
-enum tap_dance_codes {
-  DANCE_0,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    TD(DANCE_0),    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    CW_TOGG,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-    MT(MOD_LSFT, KC_BSPC),KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,      MT(MOD_RSFT, KC_QUOTE),
-    KC_LGUI,        MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_RCTL,
-                                                    LT(1,KC_ENTER), MT(MOD_LCTL, KC_TAB),                                MT(MOD_LSFT, KC_BSPC),LT(2,KC_SPACE)
+    KC_DLR,         KC_PLUS,        KC_LBRC,        KC_LCBR,        KC_LPRN,        KC_AMPR,                                        KC_EQL,         KC_RPRN,        KC_RCBR,        KC_RBRC,        KC_ASTR,        KC_EXLM,
+    KC_AT,          KC_SCLN,        KC_COMMA,       KC_DOT,         KC_P,           KC_Y,                                           KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           KC_SLASH,
+    KC_BSLS,        MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_O),MT(MOD_LCTL, KC_E),MT(MOD_LSFT, KC_U),KC_I,                               KC_D,           MT(MOD_RSFT, KC_H),MT(MOD_RCTL, KC_T),MT(MOD_LALT, KC_N),MT(MOD_RGUI, KC_S),KC_MINUS,
+    KC_PIPE,        KC_QUOTE,       KC_Q,           KC_J,           KC_K,           KC_X,                                           KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           MO(1),
+                                                    KC_BSPC,        LT(1,KC_ESCAPE),                                KC_ENTER,       KC_SPACE
   ),
   [1] = LAYOUT_voyager(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-    KC_GRAVE,       KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,                                        KC_7,           KC_8,           KC_9,           KC_MINUS,       KC_SLASH,       KC_F12,
-    KC_TRANSPARENT, KC_CIRC,        KC_AMPR,        KC_ASTR,        KC_LPRN,        KC_RPRN,                                        KC_4,           KC_5,           KC_6,           KC_PLUS,        KC_ASTR,        KC_BSPC,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_LBRC,    KC_RBRC,    KC_LCBR,        KC_RCBR,                                        KC_1,           KC_2,           KC_3,           KC_DOT,         KC_EQUAL,       KC_ENTER,
-                                                    KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_0
-  ),
-  [2] = LAYOUT_voyager(
-    RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MOD,        RGB_SLD,        RGB_VAD,        RGB_VAI,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT,                                 KC_PGUP,        KC_HOME,        KC_UP,          KC_END,         KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,                                 KC_PGDN,      KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, HSV_0_255_255,  HSV_74_255_255, HSV_169_255_255,                                KC_TRANSPARENT, LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    RGB_SPD,        RGB_SLD,        RGB_MODE_FORWARD,RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_SPI,                                        KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_PSCR,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          RGB_HUI,                                        KC_TRANSPARENT, KC_HOME,        KC_UP,          KC_END,         KC_TRANSPARENT, RGB_SAI,
+    KC_TRANSPARENT, MT(MOD_LGUI, KC_F5),MT(MOD_LALT, KC_F6),MT(MOD_LCTL, KC_F7),MT(MOD_LSFT, KC_F8),RGB_HUD,                                        KC_TRANSPARENT, MT(MOD_RSFT, KC_LEFT),MT(MOD_RCTL, KC_DOWN),MT(MOD_LALT, KC_RIGHT),MT(MOD_RGUI, KC_DELETE),RGB_SAD,
+    RGB_VAD,        KC_F9,          KC_F10,         KC_F11,         KC_F12,         RGB_VAI,                                        KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP,KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
 
-const uint16_t PROGMEM combo0[] = { MT(MOD_RSFT, KC_QUOTE), MT(MOD_LSFT, KC_BSPC), COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-    COMBO(combo0, KC_CAPS),
+const custom_shift_key_t custom_shift_keys[] = {
+	{ KC_DLR,  KC_TILD },
+	{ KC_PLUS, KC_1},
+	{ KC_LBRC, KC_2 },
+	{ KC_LCBR, KC_3 },
+	{ KC_LPRN, KC_4   },
+	{ KC_AMPR, KC_5   },
+	{ KC_EQL,  KC_6 },
+	{ KC_RPRN, KC_7 },
+	{ KC_RCBR, KC_8 },
+	{ KC_RBRC, KC_9 },
+	{ KC_ASTR, KC_0 },
+	{ KC_EXLM, KC_PERC }, /* '( */
+	{ KC_PIPE, KC_GRAVE }, /* ") */
+	{ KC_AT, KC_CIRC }, /* ,; */
+	{ KC_BSLS,  KC_HASH }, /* .: */
+	{ KC_BSPC, KC_DEL }, /* ?! */
+	{ KC_SPACE, KC_TAB }, /* ?! */
 };
 
+// Alt keys
+const key_override_t alt_one = ko_make_basic(MOD_MASK_ALT, KC_PLUS, KC_1);
+const key_override_t alt_two = ko_make_basic(MOD_MASK_ALT, KC_LBRC, KC_2);
+const key_override_t alt_three = ko_make_basic(MOD_MASK_ALT, KC_LCBR, KC_3);
+const key_override_t alt_four = ko_make_basic(MOD_MASK_ALT, KC_LPRN, KC_4);
+const key_override_t alt_five = ko_make_basic(MOD_MASK_ALT, KC_AMPR, KC_5);
+const key_override_t alt_six = ko_make_basic(MOD_MASK_ALT, KC_EQL, KC_6);
+const key_override_t alt_seven = ko_make_basic(MOD_MASK_ALT, KC_RPRN, KC_7);
+const key_override_t alt_eight = ko_make_basic(MOD_MASK_ALT, KC_RCBR, KC_8);
+const key_override_t alt_nine = ko_make_basic(MOD_MASK_ALT, KC_RBRC, KC_9);
+const key_override_t alt_zero = ko_make_basic(MOD_MASK_ALT, KC_ASTR, KC_0);
+
+// CTRL keys
+const key_override_t ctrl_one = ko_make_basic(MOD_MASK_CTRL, KC_PLUS, KC_1);
+const key_override_t ctrl_two = ko_make_basic(MOD_MASK_CTRL, KC_LBRC, KC_2);
+const key_override_t ctrl_three = ko_make_basic(MOD_MASK_CTRL, KC_LCBR, KC_3);
+const key_override_t ctrl_four = ko_make_basic(MOD_MASK_CTRL, KC_LPRN, KC_4);
+const key_override_t ctrl_five = ko_make_basic(MOD_MASK_CTRL, KC_AMPR, KC_5);
+const key_override_t ctrl_six = ko_make_basic(MOD_MASK_CTRL, KC_EQL, KC_6);
+const key_override_t ctrl_seven = ko_make_basic(MOD_MASK_CTRL, KC_RPRN, KC_7);
+const key_override_t ctrl_eight = ko_make_basic(MOD_MASK_CTRL, KC_RCBR, KC_8);
+const key_override_t ctrl_nine = ko_make_basic(MOD_MASK_CTRL, KC_RBRC, KC_9);
+const key_override_t ctrl_zero = ko_make_basic(MOD_MASK_CTRL, KC_ASTR, KC_0);
+
+// Command keys
+const key_override_t command_one = ko_make_basic(MOD_MASK_GUI, KC_PLUS, KC_1);
+const key_override_t command_two = ko_make_basic(MOD_MASK_GUI, KC_LBRC, KC_2);
+const key_override_t command_three = ko_make_basic(MOD_MASK_GUI, KC_LCBR, KC_3);
+const key_override_t command_four = ko_make_basic(MOD_MASK_GUI, KC_LPRN, KC_4);
+const key_override_t command_five = ko_make_basic(MOD_MASK_GUI, KC_AMPR, KC_5);
+const key_override_t command_six = ko_make_basic(MOD_MASK_GUI, KC_EQL, KC_6);
+const key_override_t command_seven = ko_make_basic(MOD_MASK_GUI, KC_RPRN, KC_7);
+const key_override_t command_eight = ko_make_basic(MOD_MASK_GUI, KC_RCBR, KC_8);
+const key_override_t command_nine = ko_make_basic(MOD_MASK_GUI, KC_RBRC, KC_9);
+const key_override_t command_zero = ko_make_basic(MOD_MASK_GUI, KC_ASTR, KC_0);
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case MT(MOD_LCTL, KC_E):
+            return TAPPING_TERM -25;
+        case MT(MOD_LSFT, KC_U):
+            return TAPPING_TERM -25;
+        case MT(MOD_RSFT, KC_H):
+            return TAPPING_TERM -25;
+        case MT(MOD_RCTL, KC_T):
+            return TAPPING_TERM -25;
+        default:
+            return TAPPING_TERM;
+    }
+}
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_custom_shift_keys(keycode, record)) { return false; }
   switch (keycode) {
 
     case RGB_SLD:
@@ -77,96 +106,43 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(1);
       }
       return false;
-    case HSV_0_255_255:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-        rgblight_sethsv(0,255,255);
-      }
-      return false;
-    case HSV_74_255_255:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-        rgblight_sethsv(74,255,255);
-      }
-      return false;
-    case HSV_169_255_255:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-        rgblight_sethsv(169,255,255);
-      }
-      return false;
   }
   return true;
 }
 
-
-typedef struct {
-    bool is_press_action;
-    uint8_t step;
-} tap;
-
-enum {
-    SINGLE_TAP = 1,
-    SINGLE_HOLD,
-    DOUBLE_TAP,
-    DOUBLE_HOLD,
-    DOUBLE_SINGLE_TAP,
-    MORE_TAPS
+const key_override_t **key_overrides = (const key_override_t *[]){
+  &alt_one,
+  &alt_two,
+  &alt_three,
+  &alt_four,
+  &alt_five,
+  &alt_six,
+  &alt_seven,
+  &alt_eight,
+  &alt_nine,
+  &alt_zero,
+  &ctrl_one,
+  &ctrl_two,
+  &ctrl_three,
+  &ctrl_four,
+  &ctrl_five,
+  &ctrl_six,
+  &ctrl_seven,
+  &ctrl_eight,
+  &ctrl_nine,
+  &ctrl_zero,
+  &command_one,
+  &command_two,
+  &command_three,
+  &command_four,
+  &command_five,
+  &command_six,
+  &command_seven,
+  &command_eight,
+  &command_nine,
+  &command_zero,
+  NULL
 };
 
-static tap dance_state[1];
-
-uint8_t dance_step(tap_dance_state_t *state);
-
-uint8_t dance_step(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (state->interrupted || !state->pressed) return SINGLE_TAP;
-        else return SINGLE_HOLD;
-    } else if (state->count == 2) {
-        if (state->interrupted) return DOUBLE_SINGLE_TAP;
-        else if (state->pressed) return DOUBLE_HOLD;
-        else return DOUBLE_TAP;
-    }
-    return MORE_TAPS;
-}
-
-
-void on_dance_0(tap_dance_state_t *state, void *user_data);
-void dance_0_finished(tap_dance_state_t *state, void *user_data);
-void dance_0_reset(tap_dance_state_t *state, void *user_data);
-
-void on_dance_0(tap_dance_state_t *state, void *user_data) {
-    if(state->count == 3) {
-        tap_code16(KC_EQUAL);
-        tap_code16(KC_EQUAL);
-        tap_code16(KC_EQUAL);
-    }
-    if(state->count > 3) {
-        tap_code16(KC_EQUAL);
-    }
-}
-
-void dance_0_finished(tap_dance_state_t *state, void *user_data) {
-    dance_state[0].step = dance_step(state);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: register_code16(KC_EQUAL); break;
-        case SINGLE_HOLD: register_code16(KC_ESCAPE); break;
-        case DOUBLE_TAP: register_code16(KC_EQUAL); register_code16(KC_EQUAL); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(KC_EQUAL); register_code16(KC_EQUAL);
-    }
-}
-
-void dance_0_reset(tap_dance_state_t *state, void *user_data) {
-    wait_ms(10);
-    switch (dance_state[0].step) {
-        case SINGLE_TAP: unregister_code16(KC_EQUAL); break;
-        case SINGLE_HOLD: unregister_code16(KC_ESCAPE); break;
-        case DOUBLE_TAP: unregister_code16(KC_EQUAL); break;
-        case DOUBLE_SINGLE_TAP: unregister_code16(KC_EQUAL); break;
-    }
-    dance_state[0].step = 0;
-}
-
-tap_dance_action_t tap_dance_actions[] = {
-        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
-};
+uint8_t NUM_CUSTOM_SHIFT_KEYS =
+    sizeof(custom_shift_keys) / sizeof(custom_shift_key_t);
